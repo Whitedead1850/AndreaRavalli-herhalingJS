6eb3b2cecaf3cf011637fc8e7c4eefcf20180233
Whitedead1850 <60175572+Whitedead1850@users.noreply.github.com>
2020-03-02 11:11:39 +0100

    .

---
+++ 03-wk2018.js
@@ -0,0 +1,8 @@
+function toonWinnaars() {
+    winnaars = new Array("Frankrijk", "Kroatie", "Belgie")
+    document.write("Op plaats 1 staat : " + winnaars[0])
+    document.write("<br>Op plaats 2 staat : " + winnaars[1])
+    document.write("<br>Op plaats 3 staat : " + winnaars[2])
+}
+
+toonWinnaars();
